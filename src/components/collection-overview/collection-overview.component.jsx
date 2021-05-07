3f734b510c6a73b76eb292015b59802e3831c759
krutik0204 <72077012+krutik0204@users.noreply.github.com>
2021-05-07 13:38:25 +0530

    updated with heroku, mad the project live

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -9,7 +9,7 @@ import './collection-overview.styles.scss';
 const CollectionsOverview = ({ collections }) => (
     <div className='collection-overview'>
      {
-                collections.map(({ id, ...otherCollectionProps}) => (
+                collections.map(({id, ...otherCollectionProps}) => (
                     <CollectionPreview key={id} {...otherCollectionProps} />
                 ))
             }
